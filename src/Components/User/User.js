13da9254640ec58c3a5d53757dd5e9bf3189b062
guirecordon <guilherme.recordon@gmail.com>
2022-06-01 14:40:31 -0300

    Add endless scroll to feed

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Feed from '../../Feed/Feed'
+import { UserContext } from '../../UserContext'
 import UserHeader from './UserHeader'
 import UserPhotoPost from './UserPhotoPost'
 import UserStats from './UserStats'
 
 const User = () => {
+  const {data} = React.useContext(UserContext)
+
   return <section>
     <UserHeader />
     <Routes>
-      <Route path='/' element={<Feed />} />
+      <Route path='/' element={<Feed user={data.id} />} />
       <Route path='postar' element={<UserPhotoPost />} />
       <Route path='estatistica' element={<UserStats />} />
     </Routes>
--- src/Feed/FeedPhotos.js
+++ src/Feed/FeedPhotos.js
@@ -6,18 +6,20 @@ import useFetch from '../Hooks/useFetch'
 import FeedPhotosItem from './FeedPhotosItem'
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({page, user, setModalPhoto, setInfinite}) => {
 
   const {data, loading, error, request} = useFetch()
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0})
+      let total = 3
+      const {url, options} = PHOTOS_GET({page, total, user})
       const {response, json} = await request(url, options)
       console.log(json);
+      if(response && response.ok && json.length < total) setInfinite(false)
     }
     fetchPhotos()
-  }, [request])
+  }, [request, user, page, setInfinite])
 
   if(error) return <Error error={error} />
   if(loading) return <Loading />
