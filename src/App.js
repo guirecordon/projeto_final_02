dbdb4ec10b992c37b606c6a63705c5bd8ba719f3
guirecordon <guilherme.recordon@gmail.com>
2022-06-01 15:56:58 -0300

    Add single photo styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Photo/Photo'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Route path='conta/*' element={<ProtectedRoute>
                 <User />
               </ProtectedRoute>} />
+              <Route path='foto/:id' element={<Photo />} />
             </Routes> 
           <Footer />
         </UserStorage>
--- src/Photo/Photo.js
+++ src/Photo/Photo.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import { PHOTO_GET } from '../Api'
+import useFetch from '../Hooks/useFetch'
+import Error from '../Components/Helper/Error'
+import Loading from '../Components/Helper/Loading'
+import PhotoContent from '../Photo/PhotoContent'
 
 const Photo = () => {
-  return (
-    <div>Photo</div>
-  )
+  const {id} = useParams()
+  const {data, loading, error, request} = useFetch()
+
+  React.useEffect(() => {
+    const {url, options} = PHOTO_GET(id)
+    request(url, options) 
+  }, [id, request])
+
+  if(error) return <Error error={error} />
+  if(loading) return <Loading />
+  if(data) return <section className='container mainContainer'>
+    <PhotoContent data={data} single={true} />
+    </section>
+  else return null
 }
 
 export default Photo
