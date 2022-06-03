f95a5a870df777ef40bdcd3a761175e12096b627
Gui Recordon <guilherme.recordon@gmail.com>
2022-06-03 19:31:58 -0300

    Add icons etc for build

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -37,7 +37,7 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        <Error error={error} />
+        <Error error={error && 'Dados incorretos.'} />
       </form>
       <Link className={styles.perdeu} to="/login/perdeu">
         Esqueceu a senha?
